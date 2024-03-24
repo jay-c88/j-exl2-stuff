96dfa28932994eff178168d24be75c83d666ff8c
jayc88 <zorggy@proton.me>
2024-03-24 14:11:14 +0100

    asdf

--- models/hf_up.py
+++ models/hf_up.py
@@ -12,30 +12,33 @@ def parseargs():
     return parser.parse_args()
 
 if __name__ == "__main__":
-    args = parseargs()
-    
-    upload_command = ['huggingface-cli', 'upload', '--private']
-    
-    if args.token is not None:
-        upload_command.extend(['--token', args.token])
-    
-    repo = args.repo
-    while repo is None or repo.strip() == "":
-        repo = input("Please specify a repo: ")
-    upload_command.append(repo)
+    try:
+        args = parseargs()
+        
+        upload_command = ['huggingface-cli', 'upload', '--private']
+        
+        if args.token is not None:
+            upload_command.extend(['--token', args.token])
+        
+        repo = args.repo
+        while repo is None or repo.strip() == "":
+            repo = input("Please specify a repo: ")
+        upload_command.append(repo)
 
-    f_input = args.i
-    while f_input is None:
-        f_input = input("Enter a file/folder to upload. (Leave empty to upload current folder): ")
-    if f_input != "":
-        upload_command.append(f_input)
+        f_input = args.i
+        while f_input is None:
+            f_input = input("Enter a file/folder to upload. (Leave empty to upload current folder): ")
+        if f_input != "":
+            upload_command.append(f_input)
+        
+        if args.path_repo is not None:
+            upload_command.append(args.path_repo)
+        
+        print(' '.join(upload_command))
+        subprocess.run(' '.join(upload_command), shell=True)
+    except:
+        input("Something went wrong. Press Enter to exit...")
+        sys.exit()
     
-    if args.path_repo is not None:
-        upload_command.append(args.path_repo)
-    
-    print(' '.join(upload_command))
-    print(upload_command)
-    subprocess.run(upload_command, shell=True)
-    
-    input("Done. Press Enter to exit.")
+    input("Done. Press Enter to exit...")
     sys.exit()
