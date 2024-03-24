1005e99a5d649a6e3974c64aa7e334e7cc7e35a1
jayc88 <zorggy@proton.me>
2024-03-24 07:03:36 +0100

    hf_up dont close

--- models/hf_up.py
+++ models/hf_up.py
@@ -1,5 +1,6 @@
 import argparse
 import subprocess
+import sys
 
 parser = argparse.ArgumentParser()
 
@@ -11,27 +12,34 @@ def parseargs():
     return parser.parse_args()
 
 if __name__ == "__main__":
-    args = parseargs()
-    
-    upload_command = ['huggingface-cli', 'upload']
-    
-    repo = args.repo
-    while repo is None or repo.strip() == "":
-        repo = input("Please specify a repo: ")
-    upload_command.append(repo)
+    try:
+        args = parseargs()
+        
+        upload_command = ['huggingface-cli', 'upload']
+        
+        repo = args.repo
+        while repo is None or repo.strip() == "":
+            repo = input("Please specify a repo: ")
+        upload_command.append(repo)
 
-    f_input = args.i
-    while f_input is None:
-        f_input = input("No or invalid file/folder specified. (Leave empty to upload current folder): ")
-    if f_input != "":
-        upload_command.append(f_input)
+        f_input = args.i
+        while f_input is None:
+            f_input = input("No or invalid file/folder specified. (Leave empty to upload current folder): ")
+        if f_input != "":
+            upload_command.append(f_input)
+        
+        if args.path_repo is not None:
+            upload_command.append(args.path_repo)
+        
+        upload_command.append('--private') # if repo doesn't exist, create a private repo instead of public
+        
+        if args.token is not None:
+            upload_command.extend(['--token', args.token])
+        
+        subprocess.run(upload_command, shell=True)
+    except:
+        input("Something went wrong. Press Enter to exit...")
+        sys.exit()
     
-    if args.path_repo is not None:
-        upload_command.append(args.path_repo)
-    
-    upload_command.append('--private') # if repo doesn't exist, create a private repo instead of public
-    
-    if args.token is not None:
-        upload_command.extend(['--token', args.token])
-    
-    subprocess.run(upload_command, shell=True)
+    input("Done. Press Enter to exit.")
+    sys.exit()
