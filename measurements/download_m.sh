6785892b2bb721a178fda01898f8c7dab4231789
jayc88 <zorggy@proton.me>
2024-03-26 03:06:55 +0100

    a

--- measurements/download_m.sh
+++ measurements/download_m.sh
@@ -1,2 +1,7 @@
-#/bin/bash
+#!/bin/bash
 
+REPO="JayhC/measurements"
+
+read -e -p "Measurement file to download: " MFILE
+
+huggingface-cli download --local-dir . --local-dir-use-symlinks False --token $HUGGINGFACE_TOKEN "$REPO" "$MFILE"
--- measurements/upload_m.sh
+++ measurements/upload_m.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
 
+REPO="JayhC/measurements"
+
+read -e -p "Measurement file to upload: " MFILE
+
+huggingface-cli upload --private --token $HUGGINGFACE_TOKEN "$REPO" "$MFILE"
